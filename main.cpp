30e54e90b4826f67030fd1a00b0ed2844a1f97e1
Lw-Cui <645739460@QQ.com>
2015-11-22 10:33:10 +0800

    update test

--- main.cpp
+++ main.cpp
@@ -2,39 +2,30 @@
 #include "polynomial.h"
 using namespace std;
 
-void test1() {
-	polynomial p1, p2;
+void add_and_minus(polynomial &p1, polynomial &p2) {
+    cout << "p1 = " << p1 << endl << "p2 = " << p2 << endl;
 
-	for (int i = 0; i < 5; i++)
-		p1.add(i, i + 1);
-	cout << -p1;
-
-	for (int i = 7; i > 4; i--)
-		p2.add(i, i - 1);
-
-	cout << p2 * p1;
+	cout << "p1 + p2 = " << p1 + p2 << endl;
+    cout << "p1 - p2 = " << p1 - p2 << endl;
 }
 
+void divide_and_modulo(polynomial &p1, polynomial &p2) {
+    cout << "p1 = " << p1 << endl << "p2 = " << p2 << endl;
 
-void test3() {
-	polynomial p1, p2;
-	p1.add("10x^2").add("1x^1");
-	p2.add("5x^1");
-	
-	cout << p1 / p2 << endl;
-	cout << p1 % p2 << endl;
-}
-
-void test4() {
-	polynomial p1, p2;
-	
-	p1.add("-2x^2").add("1x^1");
-	p2.add("2x^1").add("1x^0");
-	p1 = p1 / p2;
-	cout << p1 << endl;
+    cout << "p1 * p2 = " << p1 * p2 << endl;
+	cout << "p1 / p2 = " << p1 / p2 << endl;
+    cout << "p1 % p2 = " << p1 % p2 << endl;
 }
 
 int main(int args, char **agrv) { 
-	test3();
+    polynomial p1, p2;
+    
+	p1.add("-2x^2").add("1x^1");
+	p2.add("2x^1").add("1x^0");
+    add_and_minus(p1, p2);
+
+	p1.add("10x^4").add("1x^3");
+	p2.add("5x^2");
+	divide_and_modulo(p1, p2);
 	return 0;
 }
